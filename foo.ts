68b87af048cba25960b70aaeb8bffb77cecbc22b
Manuel Arturo Echeandia Pais <MECHEANDIAPAIS01@mynbcc.ca>
2023-11-06 23:31:12 -0400

    Ex2

---
+++ foo.ts
@@ -0,0 +1,34 @@
+export class Foo{
+
+    constructor(public name: string){
+        this.name = name;
+    }
+
+    fire() : void {
+        console.log("BANG!");
+        
+    }
+
+    fireWeapon(weapon: Foo.Weapons) : void {
+        console.log(`Fire the ${weapon}`);
+    }
+
+    fireWeaponToString(weapon: Foo.Weapons):string{
+        return Foo.Weapons[weapon];
+    }
+}
+
+export namespace Foo{
+    export enum Weapons{Cannon, Torpedos, MachineGun, DeathRay, NailGun};
+}
+
+let myFirstFoo = new Foo('Lenny');
+
+console.log(`I am Foo and my name is ${myFirstFoo.name}`);
+myFirstFoo.fire();
+myFirstFoo.fireWeapon(Foo.Weapons.Cannon);
+
+let theWeapon:string;
+
+theWeapon = myFirstFoo.fireWeaponToString(Foo.Weapons.MachineGun);
+console.log(`The weapon fired from toString is ${theWeapon}`);
